6195bb1eb6194db056d63da0791e7a57d2117ffc
Vinotti <mateusvinotti@gmail.com>
2024-03-25 20:14:27 -0300

    Exercicio18

---
+++ Exercicio18.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio18 {
+
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        System.out.print("Digite a terceira nota: ");
+        double nota3 = scanner.nextDouble();
+
+        double mediaHarmonica = 3 / ((1 / nota1) + (1 / nota2) + (1 / nota3));
+
+        System.out.printf("Média harmônica: %.2f\n", mediaHarmonica);
+
+        scanner.close();
+    }
+
+}
