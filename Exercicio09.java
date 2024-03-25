ed0a3687ec2f69edec0adc4d20a43c02d4dfb5a7
Vinotti <mateusvinotti@gmail.com>
2024-03-25 19:51:47 -0300

    Exercicio09

---
+++ Exercicio09.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Exercicio09 {
+
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor de A: ");
+        int A = scanner.nextInt();
+
+        System.out.print("Digite o valor de B: ");
+        int B = scanner.nextInt();
+
+        if (A == B) {
+            System.out.println("Os números são iguais.");
+        } else {
+            System.out.println("Os números são diferentes.");
+            if (A > B) {
+                System.out.println("O número A é maior que o número B.");
+            } else {
+                System.out.println("O número B é maior que o número A.");
+            }
+        }
+
+        scanner.close();
+    }
+
+}
