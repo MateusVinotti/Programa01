dbc04fafaf596772ff4596eb1874e6a40b372033
Vinotti <mateusvinotti@gmail.com>
2024-03-25 19:45:44 -0300

    Exercicio05

---
+++ Exercicio05.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Exercicio05 {
+
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o valor da variável A: ");
+        int A = scanner.nextInt();
+
+        System.out.print("Digite o valor da variável B: ");
+        int B = scanner.nextInt();
+
+        System.out.println("Valores originais:");
+        System.out.println("A: " + A);
+        System.out.println("B: " + B);
+
+        int temp = A;
+        A = B;
+        B = temp;
+
+        System.out.println("\nValores trocados:");
+        System.out.println("A: " + A);
+        System.out.println("B: " + B);
+
+        scanner.close();
+    }
+
+}
